838bc40658b7829386d42324c9766e43372b9710
Thomas Dusterwald <dsttho002@sl-dual-93.cs.uct.ac.za>
2014-05-09 14:49:31 +0200

    first commit

---
+++ CryptoMachine.cpp
@@ -0,0 +1,9 @@
+//CryptoMachine Class
+//Thomas Dusterwald
+//9 May 2014
+
+template <typename cipher, typename mode, typename group, typename pack> 
+class CrytoMachine
+{
+	static char[] alphabet = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+}
